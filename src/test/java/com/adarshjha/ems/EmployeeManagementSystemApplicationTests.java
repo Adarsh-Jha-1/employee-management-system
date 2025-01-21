f3b43ed5084441541b5377cf8984896275a9052d
Adarsh Jha <adarshjha163@gmail.com>
2025-01-21 11:00:36 +0530

    Initial commit

---
+++ src/test/java/com/adarshjha/ems/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adarshjha.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
